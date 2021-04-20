f98c603ebff7509947b05fdaf551748c08361fd0
José Carlos <josecarlosbsilva.17@gmail.com>
2021-04-20 09:14:33 -0300

    create validations for user model attributes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,3 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
 
-    render json: @users, 
-           each_serializer: User::Index::UserSerializer, status: :ok  
-  end
 end
