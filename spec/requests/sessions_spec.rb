64a124ea0f3f6b65378f4d284202f6d55870e9a0
José Carlos <josecarlosbsilva.17@gmail.com>
2021-04-21 16:54:16 -0300

    add the tests for user authentication

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,7 +1,38 @@
 require 'rails_helper'
+require 'support/json_body'
+require 'support/request_helper'
 
 RSpec.describe "Sessions", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST #new" do
+    context 'when the user is logged' do
+      it 'must return 200 status code' do
+        user = create(:user)
+        user_params = { email: user.email, password: user.password }
+
+        post "/auth/sign_in", params: user_params
+
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'must return user logged' do
+        user = create(:user)
+        user_params = { email: user.email, password: user.password }
+
+        post "/auth/sign_in", params: user_params
+        
+        expect(json_body[:data]).to have_key(:email)
+        expect(json_body[:data]).to have_key(:name)
+      end
+    end
+
+    context 'when the user is not logged' do
+      it 'must return 401 status code' do
+        user_params = { email: 'john doe', password: 'john doe' }
+
+        post "/auth/sign_in", params: user_params
+
+        expect(response).to have_http_status(:unauthorized) 
+      end
+    end
   end
 end
